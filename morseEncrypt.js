b6bc811276f14487cf881a4b4033d1e76f49c476
Ken Doman <raykendo@gmail.com>
2012-02-27 23:36:44 -0500

    added z-index, commenting, cleared up code.

--- morseEncrypt.js
+++ morseEncrypt.js
@@ -1,11 +1,21 @@
 /**
- *
+ * morseEncrypt.js
+ * -- --- .-. ... . . -. -.-. .-. -.-- .--. - .-.-.- .--- ... 
+ * Morse Code Encryption Bookmarklet.
+ * Translates text you've entered on a web page's textarea into morse code.
+ * If no text area is provided, this adds a blank on the page where you can
+ * type your message and click "Encrypt" to turn it to morse code.
+ * Tested on Firefox 10+
+ * Let me know what else it works/doesn't work with.
  */
 (function () {
 	'use strict';
-	var d = function () {return document.getElementsByTagName('textarea'); },
-		m = function () {
-			var t = {
+	var d = document, // document shortener
+		getTextArea = function () {return d.getElementsByTagName('textarea'); },
+		morse = function () {
+			// replaces all textarea element contents with 
+			// the equivalent in morse code.
+			var translator = { // key/value pairs for translation to morse code
 				A: ".-",
 				B: "-...",
 				C: "-.-.",
@@ -54,28 +64,37 @@
 				"_": "..--.-",
 				" ": " /"
 			},
-			a=d(),
-			b,
-			i, 
-			j;
-			for (i=0; i < a.length; i++) {
-				b = a[i].value.toUpperCase().split("");
-				for (j=0; j < b.length; j++) {
-					b[j] = t[b[j]] + " ";
+				textAreas = getTextArea(),
+				letters,
+				i,
+				j;
+			for (i = 0; i < textAreas.length; i++) {
+				letters = textAreas[i].value.toUpperCase().split("");
+				for (j = 0; j < letters.length; j++) {
+					letters[j] = translator[letters[j]] + " ";
 				}
-				a[i].value = b.join("");
+				textAreas[i].value = letters.join("");
 			}
 		},
 		x,
-		y = document;
-		if (d().length<1) {
-			x = y.createElement("div");
-			x.style.height="50px";x.style.width="200px";x.style.position="fixed";x.style.top="0";x.style.right="0";
-			x.innerHTML="<textarea></textarea>Morse:&nbsp;<button id='morseEnc'>Encrypt</button>";
-			y.body.appendChild(x);
-			y.getElementById("morseEnc").onclick=m;
-		} else {
-			m();
+		styles = { // css styling for morse encryption form
+			height: "50px",
+			width: "200px",
+			position: "fixed",
+			top: "0",
+			right: "0",
+			zIndex: "9999"
+		},
+		s;
+	if (getTextArea().length < 1) { // if there are no textareas...
+		x = d.createElement("div");
+		for (s in styles) {
+			x.style[s] = styles[s];
 		}
-	}	
-)();
+		x.innerHTML = "<textarea></textarea>Morse:&nbsp;<button id='morseEnc'>Encrypt</button>";
+		d.body.appendChild(x);
+		d.getElementById("morseEnc").onclick = morse;
+	} else {
+		morse(); // replaces all textarea contents with morse code.
+	}
+})();
